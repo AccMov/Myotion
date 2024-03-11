63650c6c74a8516b55aa67e434b1f0784aa75b60
dexter <dexter@email.com>
2024-03-11 19:59:51 -0400

    render grid in opengl widget

--- modules/kinematics/base.py
+++ modules/kinematics/base.py
@@ -1,12 +1,18 @@
 import time
+from PySide6.QtGui import QMouseEvent
 from PySide6.QtOpenGLWidgets import QOpenGLWidget
 from PySide6.QtCore import QTimer
+
+from modules.kinematics.input import Input
+from PySide6.QtCore import Qt
 
 
 class Base(QOpenGLWidget):
     def __init__(self,parent=None) -> None:
         super().__init__(parent)
         self.setObjectName("Base")
+        self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
+        self.input=Input()
         self.timer = QTimer()
         self.timer.timeout.connect(self.update)
         self.fps = 60
@@ -21,8 +27,23 @@ class Base(QOpenGLWidget):
         self.time += self.deltaTime
         self.lastTime = time.time()
         # print("FPS: ", 1 / self.deltaTime)
+        self.input.update()
 
     def update(self):
         super().update()
-        self.hide()
-        self.show()
+        # self.hide()
+        # self.show()
+    
+    def keyPressEvent(self, event):
+        self.input.receiveKeyEvent(event.key(), event.type())
+        self.update()
+
+    def keyReleaseEvent(self, event):
+        self.input.receiveKeyEvent(event.key(), event.type())
+        self.update() 
+    
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        self.input.mosePressEvent(event)
+    
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        return super().mouseMoveEvent(event)

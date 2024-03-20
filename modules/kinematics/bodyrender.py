2a961e243283d4eaee2e738a2b78a21e87ed6c26
dexter <dexter@email.com>
2024-03-20 19:54:20 -0400

    camera control

--- modules/kinematics/bodyrender.py
+++ modules/kinematics/bodyrender.py
@@ -14,11 +14,68 @@ import pyMotion as pm
 from OpenGL.GL import *
 
 
+def isrealmarker(rawlabels, label):
+    """check if a marker is a real marker by replacing the last character with 'L' 'O' 'A' 'P'
+
+    Args:
+        rawlabels (list of str): all labels in the c3d file
+        label (str): current label being investigated
+
+    Returns:
+        bool: True if the marker is a real marker
+    """
+
+    if (
+        label.endswith("L")
+        or label.endswith("O")
+        or label.endswith("A")
+        or label.endswith("P")
+    ):
+        return not (
+            label[:-1] + "L" in rawlabels
+            and label[:-1] + "O" in rawlabels
+            and label[:-1] + "A" in rawlabels
+            and label[:-1] + "P" in rawlabels
+        )
+    return True
+
+
+def ismarker(points, label):
+    """check if label is a marker
+
+    Args:
+        points (dict of listof str): dictionary of all points in the c3d file
+        label (str): current label name
+
+    Returns:
+        bool : if label is a marker
+    """
+    for key in points:
+        if key != "LABELS" and "value" in points[key] and label in points[key]["value"]:
+            return False
+    return True
+
+
 class BodyRender(Base):
     def __init__(self, parent=None) -> None:
-        self.c3d=pm.c3dFile("E:/Dev/auto-marker-label/training_data/training/caohengyi_bend.c3d.c3d")
-        print("-----------------")
-        print(self.c3d.data)
+        self.c3d = pm.c3dFile(
+            "E:/Dev/auto-marker-label/training_data/training/caohengyi_bend.c3d.c3d"
+        )
+        self.frame = self.c3d.data["frame_number"]
+        self.start = 0
+        labels = self.c3d.data["point_labels"]
+        points = self.c3d.data["points"]
+        analog = self.c3d.data["analog"]
+        self.points = []
+        for joint in points.data:
+            if isrealmarker(labels, joint):
+                self.points.append(
+                    [
+                        points.data[joint][0].xyz[0],
+                        points.data[joint][0].xyz[2],
+                        points.data[joint][0].xyz[1],
+                    ]
+                )
         super().__init__(parent)
 
     def initializeGL(self) -> None:
@@ -26,21 +83,21 @@ class BodyRender(Base):
 
         self.renderer = Renderer(self)
         self.scene = Object3D()
-        self.camera = Camera(aspectRatio=800 / 600)
-        self.camera.setPosition([100, 100, 500])
+        self.camera = Camera(aspectRatio=800 / 600, far=10000)
+        self.camera.setPosition([0, 1000, 3000])
         self.rig = MovementRig(unitsPerSecond=100)
         self.rig.add(self.camera)
         self.rig.setPosition([0, 1, 2])
         self.scene.add(self.rig)
 
         axes = AxesItem(axisLength=100)
-        grid = GridItem(size=100, gridColor=[1, 1, 1], centerColor=[1, 1, 0])
+        grid = GridItem(size=5000, gridColor=[1, 1, 1], centerColor=[1, 1, 0])
         grid.rotateX(-pi / 2)
-        point = PointItem()
+        self.point = PointItem(self.points)
 
         self.scene.add(axes)
         self.scene.add(grid)
-        self.scene.add(point)
+        self.scene.add(self.point)
 
     def paintGL(self) -> None:
         super().paintGL()

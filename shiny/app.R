9e8954c36926efc2ae2f4a6763c774253acd32db
Brox Chen <broxigarchen@outlook.com>
2024-04-06 13:59:00 -0400

    fix rserver path

--- shiny/app.R
+++ shiny/app.R
@@ -1,5 +1,12 @@
 require(shiny)
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# get path of current script
+initial.options <- commandArgs(trailingOnly = FALSE)
+file.arg.name <- "--file="
+script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+script.dirname <- dirname(script.name)
+setwd(script.dirname)
+
 source('server.R', local = TRUE)
 source('ui.R', local = TRUE)
 

2666f3514c39a3648b36f5109c1aecfbccdcdb30
kenp666 <86131590+kenp666@users.noreply.github.com>
2024-05-22 10:55:40 -0700

    Update install.R

--- shiny/install.R
+++ shiny/install.R
@@ -1,6 +1,6 @@
 pacs = c("R.matlab","stringr","ggplot2","plotly","shinycssloaders",
           "ggthemes","shinydashboardPlus","DT","shiny","shinydashboard",
           "tidyverse","flextable","fresh","rempsyc","ggridges","shinyWidgets","shinyjs",
-         "xtable","kableExtra","car")
+         "xtable","kableExtra","car","XML")
 
 install.packages(pacs, repos='http://cran.us.r-project.org')
